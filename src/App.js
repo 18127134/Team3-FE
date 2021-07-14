f1519a5cbb7bbea5934fed6d31c990bb6cabcb81
alexanderloong <dj.thaomeo@gmail.com>
2021-07-14 13:27:09 +0700

    Add homepage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from './Component/header';
 import Navbar from './Component/navbar';
 import Formsignup from './Component/form_signup';
 import Footer from './Component/footer';
+import Formsignin from './Component/form_signin';
+import Homepage from './Component/homepage';
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
-      <Formsignup />
+      <Homepage />
       <Footer />
     </div>
 
