a47f346a51161bf404d6e86b6df815887915ed60
alexanderloong <51206070+alexanderloong@users.noreply.github.com>
2021-08-13 21:57:17 +0700

    Update chooseservice.js

--- src/Component/booking/chooseservice.js
+++ src/Component/booking/chooseservice.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Service from "./service";
+import nail1 from "../../Picture/nail_1.png"
+import Topseller from "../topseller";
+
 
 // Test list service
 const nail_service = [
@@ -27,7 +30,33 @@ const nail_service = [
         id: "aa5",
         name: "Sơn mắt mèo",
         price: 90000
+    },
+    {
+        id: "aa6",
+        name: "Sơn giả bột",
+        price: 90000
+    },
+    {
+        id: "aa7",
+        name: "Sơn theo trend",
+        price: 90000
+    },
+    {
+        id: "aa8",
+        name: "Móng up",
+        price: 90000
+    },
+    {
+        id: "aa9",
+        name: "Đắp gel",
+        price: 180000
+    },
+    {
+        id: "aa10",
+        name: "Đắp bột",
+        price: 150000
     }
+
 ]
 
 const nail_design = [
@@ -45,6 +74,26 @@ const nail_design = [
         id: "bbb3",
         name: "Đá rắc",
         price: 30000
+    },
+    {
+        id: "bbb4",
+        name: "Vẽ gel theo trend",
+        price: 30000
+    },
+    {
+        id: "bbb5",
+        name: "Design bột",
+        price: 35000
+    },
+    {
+        id: "bbb6",
+        name: "Charm",
+        price: 50000
+    },
+    {
+        id: "bbb7",
+        name: "Đính đá",
+        price: 5000
     }
 ]
 
@@ -59,45 +108,64 @@ function Chooseservice() {
     const itemcheck = [];
 
     return (
-        <div className="d-flex justify-content-center">
-            <div id="container-border" className="d-flex choose-service">
-                <div>
-                    <h1>
-                        Nail Service
-                    </h1>
-
-                    {nail_service.map((item, index) => {
-                        return (
-                            <Service
-                                key={index}
-                                id={item.id}
-                                name={item.name}
-                                price={item.price}
-                                idchoose={handleChoose}
-                            />
-                        )
-                    })}
-                </div>
-
-                <div>
-                    <h1>
-                        Nail Design
-                    </h1>
-
-                    {nail_design.map((item, index) => {
-                        return (
-                            <Service
-                                key={index}
-                                id={item.id}
-                                name={item.name}
-                                price={item.price}
-                                idchoose={handleChoose}
-                            />
-                        )
-                    })}
-                </div>
-
+        <div className="d-flex justify-content-between">
+            <div className="row align-items-end">
+                <img src={nail1} class=" " alt="nail" />
             </div>
+
+            <div>
+                <h1 className="text-center">Chọn Dịch Vụ</h1>
+                <div id="container-border" className="d-flex justify-content-between choose-service">
+                    <div style={{ width: "250px" }}>
+                        <h2 className="text-center">
+                            Nail Service
+                        </h2>
+                        <div className="d-flex justify-content-between" >
+                            <h6 className="" style={{ marginTop: "0%" }}>TÊN DỊCH VỤ</h6>
+                            <h6>GIÁ TIỀN</h6>
+                        </div>
+                        {nail_service.map((item, index) => {
+                            return (
+                                <Service
+                                    key={index}
+                                    id={item.id}
+                                    name={item.name}
+                                    price={item.price}
+                                    idchoose={handleChoose}
+                                />
+                            )
+                        })}
+                    </div>
+
+                    <div style={{ width: "250px" }}>
+                        <h2 className="text-center">
+                            Nail Design
+                        </h2>
+
+                        <div className="d-flex justify-content-between" >
+                            <h6 className="" style={{ marginTop: "0%" }}>TÊN DỊCH VỤ</h6>
+                            <h6>GIÁ TIỀN</h6>
+                        </div>
+
+                        {nail_design.map((item, index) => {
+                            return (
+                                <Service
+                                    key={index}
+                                    id={item.id}
+                                    name={item.name}
+                                    price={item.price}
+                                    idchoose={handleChoose}
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className="text-center" style={{ marginBottom: "20px" }}>
+                    <button type="submit" className="btn btn-danger" > Đặt Lịch</button>
+                </div>
+            </div>
+            <Topseller />
         </div>
 
     )
