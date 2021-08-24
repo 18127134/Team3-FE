35e8a38448e90e8cea2acf6a5f1aae44076a7749
alexanderloong <51206070+alexanderloong@users.noreply.github.com>
2021-08-24 10:41:11 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
-import './App.css';
-import Header from './Component/header';
-import Navbar from './Component/navbar';
-import Formsignup from './Component/form_signup';
-import Footer from './Component/footer';
-import Formsignin from './Component/form_signin';
-import Homepage from './Component/homepage';
-import React from 'react';
-import Calendarbook from './Component/booking/calendar';
-import Chooseservice from './Component/booking/chooseservice';
-import Bookinginformation from './Component/booking/information';
-import Payment from './Component/booking/payment';
-import Mngbooking from './Component/Mngbooking';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
-
+import "./App.css";
+import React, { Fragment } from "react";
+import Customer from "./Component/Customer";
+import Manager from "./Component/Manager";
 
 function App() {
+  const type = 0;
 
-  let itemcheck = [];
-
-  return (
-    <Router>
-      <Header />
-      <Navbar status={0} />
-      <Switch>
-        <Route path="/" exact>
-          <Homepage />
-        </Route>
-        <Route path="/signin">
-          <Formsignin />
-        </Route>
-        <Route path="/signup">
-          <Formsignup />
-        </Route>
-        <Route path="/booking" exact>
-          <Calendarbook />
-        </Route>
-        <Route path="/booking/chooseservice">
-          <Chooseservice itemcheck={itemcheck} />
-        </Route>
-        <Route path="/booking/information">
-          <Bookinginformation />
-        </Route>
-        <Route path="/booking/payment">
-          <Payment />
-        </Route>
-
-        <Route path="/managementbooking">
-          <Mngbooking />
-        </Route>
-
-      </Switch>
-      <Footer />
-    </Router>
-
-  );
+  return <Fragment>{type === 1 ? <Customer /> : <Manager />}</Fragment>;
 }
 
 export default App;
-
